e122f552f66f08114e5eb962130a1ce0e089b6f7
Enzo <enzocoutinho81@gmail.com>
2020-12-13 22:42:25 -0300

    Criando pasta com os testes para tração

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testestracao/TesteField.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode.testestracao;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+@TeleOp(name="Teleoperado Under Ctrl (field oriented) 14391", group="Linear TesteOp")
+public class TesteField extends LinearOpMode {
+
+    ElapsedTime runtime = new ElapsedTime();
+
+    HardwareClassTracao hard = new HardwareClassTracao();
+    static String ladoO;
+
+    //Referência de oritenação para field Oriented
+    Orientation angles;
+
+    //Respectivamente eixos do gamepad y, x, x outro analógico
+    double drive, turn, giro;
+    double pi = 3.1415926;
+
+    //Vetor para poderes;
+    private final double[] poder = new double[4];
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //Inicia o hardware do robô
+        hard.hardwareGeral(hardwareMap);
+
+        runtime.reset();
+
+        //Espera o botão start na Ds
+        waitForStart();
+
+        while (opModeIsActive()) {
+            //Variáveis gamepad
+            drive = -gamepad1.left_stick_y;
+            turn = gamepad1.left_stick_x * 1.5;
+            giro = gamepad1.right_stick_x;
+
+            //Calculo para field oriented
+            processamentoGame(drive, turn);
+
+            //Valores para movimentação com mechanum (lados espelhados)
+            //Motor Esquerda Frente;
+            poder[0] = drive + turn + giro;
+            //Motor Esquerda trás;
+            poder[1] = drive - turn + giro;
+            //Motor Direita Frente;
+            poder[2] = drive - turn - giro;
+            //Motor Direita trás;
+            poder[3] = drive + turn - giro;
+
+            //Verificar se algum valor é maior que 1
+            if (Math.abs(poder[0]) > 1 || Math.abs(poder[1]) > 1
+                    || Math.abs(poder[2]) > 1 || Math.abs(poder[3]) > 1) {
+
+                //Achar o maior valor
+                double max;
+                max = Math.max(Math.abs(poder[0]), Math.abs(poder[1]));
+                max = Math.max(Math.abs(poder[2]), max);
+                max = Math.max(Math.abs(poder[3]), max);
+
+                //Não ultrapassar +/-1 (proporção);
+                poder[0] /= max;
+                poder[1] /= max;
+                poder[2] /= max;
+                poder[3] /= max;
+            }
+
+            //Metodo setPower que manda força para os motores.
+            hard.motorEsquerda.setPower(poder[0]);
+            hard.motorEsquerdaTras.setPower(poder[1]);
+            hard.motorDireita.setPower(poder[2]);
+            hard.motorDireitaTras.setPower(poder[3]);
+
+            //Telemetria com os valores de cada roda
+            telemetry.addData("Motor Esquerdo %.2f", poder[0]);
+            telemetry.addData("Motor EsquerdoTras %.2f", poder[1]);
+            telemetry.addData("Motor Direita %.2f", poder[2]);
+            telemetry.addData("Motor DireitaTras %.2f", poder[3]);
+            telemetry.addData("A nossa aliança é a: ", ladoO);
+
+            telemetry.update();
+        }
+    }
+
+        private void processamentoGame ( double driveP, double turnP){
+            double angle = gyroCalculate() * pi / 180;
+            drive = driveP * Math.cos(angle) - turnP * Math.sin(angle);
+            turn = driveP * Math.sin(angle) + turnP * Math.cos(angle);
+        }
+
+        private double gyroCalculate () {
+            angles = HardwareClassTracao.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            return angles.firstAngle;
+        }
+}
