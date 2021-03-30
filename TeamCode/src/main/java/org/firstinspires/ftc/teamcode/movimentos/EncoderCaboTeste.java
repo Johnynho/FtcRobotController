b67657654099db3df1ff93f65f7d60428db1c56f
lcarvalho14391 <carvgamer@gmail.com>
2021-03-30 18:39:56 -0300

    Completando encoder test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movimentos/EncoderCaboTeste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movimentos/EncoderCaboTeste.java
@@ -10,30 +10,34 @@ import org.firstinspires.ftc.teamcode.HardwareClass;
 @TeleOp(name="Teste Andar Encoder", group="Linear TesteOp")
 public class EncoderCaboTeste extends LinearOpMode {
 
+    private static final double     COUNTS_PER_MOTOR_GOBILDA  = 537.6;   //CPR * 4 * Redução
+    private static final double     DRIVE_GEAR_REDUCTION    = 1.0;      //Redução fora do motor
+    private static final double     WHEEL_DIAMETER_INCHES   = 3.7;     //Diâmetro da roda em in
+    private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_GOBILDA * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES);                        //Contagens por polegadas
+
     HardwareClass robot   = new HardwareClass();
     private final ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode(){
-        robot.hardwareGeral(hardwareMap);
-        
         waitForStart();
-        while(opModeIsActive()){
-            encoderDrive(50,10,10, 5);
-        }
+        encoderDrive(1.0,10,5);
+        int pp = (int) (robot.motorEsquerda.getCurrentPosition()/COUNTS_PER_INCH);
+        telemetry.addData("Polegadas percorridas", pp);
+        telemetry.update();
     }
 
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
+    public void encoderDrive(double speed, double leftInches, double timeoutS) {
         int newLeftTarget;
-        int newRightTarget;
+
+        newLeftTarget = robot.motorEsquerda.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+        robot.motorEsquerda.setTargetPosition(newLeftTarget);
 
         // Ensure that the opmode is still active
         if (opModeIsActive()) {
             // Turn On RUN_TO_POSITION
             robot.motorEsquerda.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.motorDireita.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             // reset the timeout time and start motion.
             runtime.reset();
@@ -41,14 +45,11 @@ public class EncoderCaboTeste extends LinearOpMode {
             robot.motorDireita.setPower(Math.abs(speed));
 
 
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (robot.motorEsquerda.isBusy() && robot.motorDireita.isBusy())) {
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (robot.motorEsquerda.isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Path2",  "Running at %7d :%7d",
-                        robot.motorEsquerda.getCurrentPosition(),
-                        robot.motorDireita.getCurrentPosition());
+                telemetry.addData("Path2",  "Running at %7d :%7d");
+                        robot.motorEsquerda.getCurrentPosition();
                 telemetry.update();
             }
 
@@ -58,7 +59,6 @@ public class EncoderCaboTeste extends LinearOpMode {
 
             // Turn off RUN_TO_POSITION
             robot.motorEsquerda.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.motorDireita.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 }
