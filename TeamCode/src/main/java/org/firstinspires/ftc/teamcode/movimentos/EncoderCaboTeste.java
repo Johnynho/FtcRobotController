fff85588756b75fda06262a6a5b14a80795f29fd
lcarvalho14391 <carvgamer@gmail.com>
2021-03-31 15:22:22 -0300

    Ajustes Encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movimentos/EncoderCaboTeste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movimentos/EncoderCaboTeste.java
@@ -1,13 +1,13 @@
 package org.firstinspires.ftc.teamcode.movimentos;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.HardwareClass;
 
-@TeleOp(name="Teste Andar Encoder", group="Linear TesteOp")
+@Autonomous(name="Teste Andar Encoder", group="Pushbot")
 public class EncoderCaboTeste extends LinearOpMode {
 
     private static final double     COUNTS_PER_MOTOR_GOBILDA  = 537.6;   //CPR * 4 * Redução
@@ -26,25 +26,24 @@ public class EncoderCaboTeste extends LinearOpMode {
 
         motorEsquerda = hardwareMap.get(DcMotor.class, "motor_Esquerda");
         motorDireita = hardwareMap.get(DcMotor.class,"motor_Direita");
-        
-        motorEsquerda.setDirection(DcMotor.Direction.FORWARD);
-        motorDireita.setDirection(DcMotor.Direction.REVERSE);
+
+        motorEsquerda.setDirection(DcMotor.Direction.REVERSE);
+        motorDireita.setDirection(DcMotor.Direction.FORWARD);
 
         motorEsquerda.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorEsquerda.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
-        encoderDrive(1.0,10,5);
-
+        encoderDrive(0.75,10,5);
         int pp = (int) (motorEsquerda.getCurrentPosition()/COUNTS_PER_INCH);
-
         telemetry.addData("Polegadas percorridas", pp);
         telemetry.update();
     }
 
     public void encoderDrive(double speed, double leftInches, double timeoutS) {
         int newLeftTarget;
+        int pp = (int) (motorEsquerda.getCurrentPosition()/COUNTS_PER_INCH);
 
         newLeftTarget = motorEsquerda.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
         motorEsquerda.setTargetPosition(newLeftTarget);
@@ -53,24 +52,23 @@ public class EncoderCaboTeste extends LinearOpMode {
         if (opModeIsActive()) {
             // Turn On RUN_TO_POSITION
             motorEsquerda.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
             // reset the timeout time and start motion.
             runtime.reset();
             motorEsquerda.setPower(Math.abs(speed));
-            motorDireita.setPower(Math.abs(speed));
 
 
             while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorEsquerda.isBusy())) {
 
+                motorDireita.setPower(Math.abs(0.56));
                 // Display it for the driver.
-                telemetry.addData("Path2",  "Running at %7d :%7d");
-                        motorEsquerda.getCurrentPosition();
+                telemetry.addData("Path2",  "Running at %7d", motorEsquerda.getCurrentPosition());
+                telemetry.addData("Polegadas percorridas", pp);
                 telemetry.update();
             }
 
             // Stop all motion;
-            motorEsquerda.setPower(0);
             motorDireita.setPower(0);
+            motorEsquerda.setPower(0);
 
             // Turn off RUN_TO_POSITION
             motorEsquerda.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
