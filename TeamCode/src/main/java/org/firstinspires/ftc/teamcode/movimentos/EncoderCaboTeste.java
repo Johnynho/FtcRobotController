9fc0ce2ad1eafff659abe67618d89610092d2b5b
lcarvalho14391 <carvgamer@gmail.com>
2021-04-06 13:44:04 -0300

    Comentários encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movimentos/EncoderCaboTeste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movimentos/EncoderCaboTeste.java
@@ -10,12 +10,14 @@ import org.firstinspires.ftc.teamcode.HardwareClass;
 @Autonomous(name="Teste Andar Encoder", group="Pushbot")
 public class EncoderCaboTeste extends LinearOpMode {
 
+    //Calculos do COUNTS_PER_INCH
     private static final double     COUNTS_PER_MOTOR_GOBILDA  = 537.6;   //CPR * 4 * Redução
     private static final double     DRIVE_GEAR_REDUCTION    = 1.0;      //Redução fora do motor
     private static final double     WHEEL_DIAMETER_INCHES   = 3.7;     //Diâmetro da roda em in
     private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_GOBILDA * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES);                        //Contagens por polegadas
 
+    //Inicialização
     HardwareClass robot   = new HardwareClass();
     private final ElapsedTime runtime = new ElapsedTime();
     DcMotor motorEsquerda, motorDireita;
@@ -24,12 +26,15 @@ public class EncoderCaboTeste extends LinearOpMode {
     @Override
     public void runOpMode(){
 
+        //Parte da inicialização
         motorEsquerda = hardwareMap.get(DcMotor.class, "motor_Esquerda");
         motorDireita = hardwareMap.get(DcMotor.class,"motor_Direita");
 
+        //Coloca as direções
         motorEsquerda.setDirection(DcMotor.Direction.REVERSE);
         motorDireita.setDirection(DcMotor.Direction.FORWARD);
 
+        //Configuração do encoder
         motorEsquerda.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorEsquerda.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
@@ -42,35 +47,36 @@ public class EncoderCaboTeste extends LinearOpMode {
     }
 
     public void encoderDrive(double speed, double leftInches, double timeoutS) {
+        int speedright = (int) (speed-0.19);
         int newLeftTarget;
         int pp = (int) (motorEsquerda.getCurrentPosition()/COUNTS_PER_INCH);
 
         newLeftTarget = motorEsquerda.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
         motorEsquerda.setTargetPosition(newLeftTarget);
 
-        // Ensure that the opmode is still active
+        //Confere se o opMode está ativo
         if (opModeIsActive()) {
-            // Turn On RUN_TO_POSITION
+            //Ativa o RUN_TO_POSITION
             motorEsquerda.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            // reset the timeout time and start motion.
+            //Reseta o runtime e começa o movimento
             runtime.reset();
             motorEsquerda.setPower(Math.abs(speed));
 
 
             while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorEsquerda.isBusy())) {
 
-                motorDireita.setPower(Math.abs(0.56));
-                // Display it for the driver.
+                motorDireita.setPower(Math.abs(speedright));
+                //Mostra para o piloto informações sobre o caminho
                 telemetry.addData("Path2",  "Running at %7d", motorEsquerda.getCurrentPosition());
                 telemetry.addData("Polegadas percorridas", pp);
                 telemetry.update();
             }
 
-            // Stop all motion;
+            //Para qualquer movimento
             motorDireita.setPower(0);
             motorEsquerda.setPower(0);
 
-            // Turn off RUN_TO_POSITION
+            //Desliga o RUN_TO_POSITION
             motorEsquerda.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
